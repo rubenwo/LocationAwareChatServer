73854e3d83166fc737844970b85038787f9090f9
Ruben <rwoldhui@avans.nl>
2019-01-15 00:22:59 +0100

    [Fixed] uncaught exception

--- src/com/Services/UserAuthenticationService.java
+++ src/com/Services/UserAuthenticationService.java
@@ -13,9 +13,12 @@ public class UserAuthenticationService {
      */
     public static User authenticate(String fireBaseToken) {
         try {
-            FirebaseToken decodedToken = FirebaseAuth.getInstance().verifyIdToken(fireBaseToken);
+            FirebaseToken decodedToken = FirebaseAuth.getInstance().verifyIdToken(fireBaseToken, true);
             return new User(decodedToken.getName(), decodedToken.getEmail(), decodedToken.getUid());
         } catch (FirebaseAuthException e) {
+            e.printStackTrace();
+            return null;
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
